d015626ec9ef700a632089e1fb8bf49742649bc2
andevishnuvardhan <50207388+andevishnuvardhan@users.noreply.github.com>
2021-06-26 18:46:27 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Footer() {
+  return <footer>Copy Right {new Date().getFullYear()}</footer>;
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Test Co-ordinaters</h1>
+      <p>Conductiong test and Collecting the test data.</p>
+    </div>
+  );
+}
+
+export default Info;
